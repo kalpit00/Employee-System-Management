95b23a1275fd3928d2b1b00ecb7d046355a2a790
kalpit00 <kalpitpatel@Kalpits-MacBook-Air.local>
2024-07-16 12:44:50 +0530

    Initial Commit

---
+++ src/test/java/com/kalpit00/Employee/EmployeeSystemApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kalpit00.Employee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeSystemApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
