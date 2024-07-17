781fb06ec580fdaea259a8c77cec6bfc6102b2cf
kalpit00 <kalpitpatel@Kalpits-MacBook-Air.local>
2024-07-17 12:20:56 +0530

    Form added. nice colors added as well

--- employee-system-ui/src/App.js
+++ employee-system-ui/src/App.js
@@ -1,8 +1,14 @@
 import "./App.css";
+import AddEmployee from "./components/AddEmployee";
 import Navbar from "./components/Navbar";
 
 function App() {
-  return <Navbar />;
+  return (
+    <div className="bg-slate-500">
+      <Navbar />
+      <AddEmployee />
+    </div>
+  );
 }
 
 export default App;
