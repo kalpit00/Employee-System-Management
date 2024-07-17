788b20974d4490bc7756833aae557fef4faf4d5c
kalpit00 <kalpitpatel@Kalpits-MacBook-Air.local>
2024-07-17 14:05:22 +0530

    Add a service which calls a function to Save Employee using Axios Post Request

--- employee-system-ui/src/components/AddEmployee.js
+++ employee-system-ui/src/components/AddEmployee.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import EmployeeService from "../services/EmployeeService";
 
 const AddEmployee = () => {
   const [employee, setEmployee] = useState({
@@ -10,6 +11,16 @@ const AddEmployee = () => {
   const handleChange = (e) => {
     const value = e.target.value;
     setEmployee({ ...employee, [e.target.name]: value });
+  };
+  const saveEmployee = (e) => {
+    e.preventDefault();
+    EmployeeService.saveEmployee(employee)
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   };
   return (
     <div className="flex max-w-2xl mx-auto shadow border-b bg-blue-400">
@@ -54,7 +65,10 @@ const AddEmployee = () => {
           ></input>
         </div>
         <div className="items-center justify-center h-14 w-full my-4 space-x-4 pt-4">
-          <button className="rounded text-white font-semibold bg-green-400 hover:bg-green-700 py-2 px-10">
+          <button
+            onClick={saveEmployee}
+            className="rounded text-white font-semibold bg-green-400 hover:bg-green-700 py-2 px-10"
+          >
             Save
           </button>
           <button className="rounded text-white font-semibold bg-red-400 hover:bg-red-700 py-2 px-10">
