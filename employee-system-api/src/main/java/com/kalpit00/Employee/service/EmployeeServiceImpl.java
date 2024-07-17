deacead3346659a0d3cedf435546cb6c5d1ceacf
kalpit00 <kalpitpatel@Kalpits-MacBook-Air.local>
2024-07-17 14:12:38 +0530

    Added a GetMapping to Fetch All Employees from backend

--- employee-system-api/src/main/java/com/kalpit00/Employee/service/EmployeeServiceImpl.java
+++ employee-system-api/src/main/java/com/kalpit00/Employee/service/EmployeeServiceImpl.java
@@ -7,6 +7,9 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
     @Autowired
@@ -18,5 +21,21 @@ public class EmployeeServiceImpl implements EmployeeService {
         BeanUtils.copyProperties(employee, employeeEntity);
         employeeRepository.save(employeeEntity);
         return employee;
+    }
+
+    @Override
+    public List<Employee> getAllEmployees() {
+        List<EmployeeEntity> employeeEntities
+                = employeeRepository.findAll();
+
+        List<Employee> employees = employeeEntities
+                .stream()
+                .map(employee -> new Employee(
+                        employee.getId(),
+                        employee.getFirstName(),
+                        employee.getLastName(),
+                        employee.getEmailId()))
+                .collect(Collectors.toList());
+        return employees;
     }
 }
