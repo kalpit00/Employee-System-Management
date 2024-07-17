76951111b39ff1fd3b016c86d4bb54707a3f43c0
kalpit00 <kalpitpatel@Kalpits-MacBook-Air.local>
2024-07-17 16:30:44 +0530

    Update and Cancel Employee implemented on Frontend + extra navigation to Fetch lists from Add Employee page without adding new

--- employee-system-ui/src/App.js
+++ employee-system-ui/src/App.js
@@ -1,19 +1,21 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import AddEmployee from "./components/AddEmployee";
+import UpdateEmployee from "./components/UpdateEmployee";
 import Navbar from "./components/Navbar";
 import EmployeeList from "./components/EmployeeList";
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="">
+      <div className="bg-teal-100">
         <Navbar />
         <Routes>
           <Route index element={<EmployeeList />} />
           <Route path="/" element={<EmployeeList />}></Route>
           <Route path="/employeeList" element={<EmployeeList />} />
           <Route path="/addEmployee" element={<AddEmployee />} />
+          <Route path="/editEmployee/:id" element={<UpdateEmployee />} />
         </Routes>
       </div>
     </BrowserRouter>
