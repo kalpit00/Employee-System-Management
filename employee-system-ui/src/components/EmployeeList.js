f3726c9a90aba5e4d7ae79bc4763774ec4ea7c2d
kalpit00 <kalpitpatel@Kalpits-MacBook-Air.local>
2024-07-17 15:34:10 +0530

    Add a useEffect Hook to fetch the employee list. set to async and await to let database fetch it. And dynamically populate the table using our employee useState hook variable

--- employee-system-ui/src/components/EmployeeList.js
+++ employee-system-ui/src/components/EmployeeList.js
@@ -1,8 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import EmployeeService from "../services/EmployeeService";
 
 const EmployeeList = () => {
   const navigate = useNavigate();
+
+  const [loading, setLoading] = useState(true);
+  const [employees, setEmployees] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const response = await EmployeeService.getEmployees();
+        setEmployees(response.data);
+      } catch (error) {
+        console.log(error);
+      }
+      setLoading(false);
+    };
+    fetchData();
+  }, []);
+
   return (
     <div className="container mx-auto my-6">
       <div className="h-12">
@@ -31,33 +50,43 @@ const EmployeeList = () => {
               </th>
             </tr>
           </thead>
-          <tbody>
-            <tr>
-              <td className="text-left px-6 py-4 whitespace-nowrap">
-                <div className="text-sm text-slate-950">Test</div>
-              </td>
-              <td className="text-left px-6 py-4 whitespace-nowrap">
-                <div className="text-sm text-slate-950">Test</div>
-              </td>
-              <td className="text-left px-6 py-4 whitespace-nowrap">
-                <div className="text-sm text-slate-950">Test</div>
-              </td>
-              <td className="text-right px-6 py-4 whitespace-nowrap font-medium">
-                <a
-                  href="#"
-                  className="text-indigo-600 hover:text-indigo-800 px-4"
-                >
-                  Edit
-                </a>
-                <a
-                  href="#"
-                  className="text-indigo-600 hover:text-indigo-800 px-4"
-                >
-                  Delete
-                </a>
-              </td>
-            </tr>
-          </tbody>
+          {!loading && (
+            <tbody>
+              {employees.map((employee) => (
+                <tr key={employee.id}>
+                  <td className="text-left px-6 py-4 whitespace-nowrap">
+                    <div className="text-sm text-slate-950">
+                      {employee.firstName}
+                    </div>
+                  </td>
+                  <td className="text-left px-6 py-4 whitespace-nowrap">
+                    <div className="text-sm text-slate-950">
+                      {employee.lastName}
+                    </div>
+                  </td>
+                  <td className="text-left px-6 py-4 whitespace-nowrap">
+                    <div className="text-sm text-slate-950">
+                      {employee.emailId}
+                    </div>
+                  </td>
+                  <td className="text-right px-6 py-4 whitespace-nowrap font-medium">
+                    <a
+                      href="#"
+                      className="text-indigo-600 hover:text-indigo-800 px-4"
+                    >
+                      Edit
+                    </a>
+                    <a
+                      href="#"
+                      className="text-indigo-600 hover:text-indigo-800 px-4"
+                    >
+                      Delete
+                    </a>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          )}
         </table>
       </div>
     </div>
