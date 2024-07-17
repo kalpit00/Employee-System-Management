3be1a181b54a1f5672db980e1214069ce5ac3b27
kalpit00 <kalpitpatel@Kalpits-MacBook-Air.local>
2024-07-17 15:54:07 +0530

    Added Delete API method on backend

--- employee-system-api/src/main/java/com/kalpit00/Employee/controller/EmployeeController.java
+++ employee-system-api/src/main/java/com/kalpit00/Employee/controller/EmployeeController.java
@@ -3,9 +3,12 @@ package com.kalpit00.Employee.controller;
 import com.kalpit00.Employee.model.Employee;
 import com.kalpit00.Employee.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @CrossOrigin(origins = "http://localhost:3000")
 @RestController
@@ -27,5 +30,14 @@ public class EmployeeController {
     @GetMapping("/employees")
     public List<Employee> getAllEmployees() {
         return  employeeService.getAllEmployees();
+    }
+
+    @DeleteMapping("/employees/{id}")
+    public ResponseEntity<Map<String,Boolean>> deleteEmployee(@PathVariable Long id) {
+        boolean deleted = false;
+        deleted = employeeService.deleteEmployee(id);
+        Map<String,Boolean> response = new HashMap<>();
+        response.put("deleted", deleted);
+        return ResponseEntity.ok(response);
     }
 }
