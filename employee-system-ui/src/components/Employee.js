5118d03b87f0e19bdc56ade982d360caabf8bbb8
kalpit00 <kalpitpatel@Kalpits-MacBook-Air.local>
2024-07-17 16:11:35 +0530

    Add the onClick for Edit employee on table, just navigate to a 3rd page for now

--- employee-system-ui/src/components/Employee.js
+++ employee-system-ui/src/components/Employee.js
@@ -1,6 +1,13 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Employee = ({ employee, deleteEmployee }) => {
+  const navigate = useNavigate();
+
+  const editEmployee = (e, id) => {
+    e.preventDefault();
+    navigate(`/editEmployee/${id}`);
+  };
   return (
     <tr key={employee.id}>
       <td className="text-left px-6 py-4 whitespace-nowrap">
@@ -13,9 +20,12 @@ const Employee = ({ employee, deleteEmployee }) => {
         <div className="text-sm text-slate-950">{employee.emailId}</div>
       </td>
       <td className="text-right px-6 py-4 whitespace-nowrap font-medium">
-        <a href="#" className="text-indigo-600 hover:text-indigo-800 px-4">
+        <button
+          onClick={(e, id) => editEmployee(e, employee.id)}
+          className="text-indigo-600 hover:text-indigo-800 px-4"
+        >
           Edit
-        </a>
+        </button>
         <button
           onClick={(e, id) => deleteEmployee(e, employee.id)}
           className="text-indigo-600 hover:text-indigo-800 px-4"
--- employee-system-ui/src/services/EmployeeService.js
+++ employee-system-ui/src/services/EmployeeService.js
@@ -12,6 +12,13 @@ class EmployeeService {
   deleteEmployee(id) {
     return axios.delete(EMPLOYEE_API_BASE_URL + "/" + id);
   }
+  getEmployeeById(id) {
+    return axios.get(EMPLOYEE_API_BASE_URL + "/" + id);
+  }
+
+  updateEmployee(employee, id) {
+    return axios.put(EMPLOYEE_API_BASE_URL + "/" + id, employee);
+  }
 }
 // eslint-disable-next-line import/no-anonymous-default-export
 export default new EmployeeService();
