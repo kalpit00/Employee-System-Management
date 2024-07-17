65163153ce16de8242892f37de20e126f192c727
kalpit00 <kalpitpatel@Kalpits-MacBook-Air.local>
2024-07-17 14:39:57 +0530

    Added routes to AddEmployee Page + to List Employee Page. Super simple routing

--- employee-system-ui/src/App.js
+++ employee-system-ui/src/App.js
@@ -1,13 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import AddEmployee from "./components/AddEmployee";
 import Navbar from "./components/Navbar";
+import EmployeeList from "./components/EmployeeList";
 
 function App() {
   return (
-    <div className="bg-slate-500">
-      <Navbar />
-      <AddEmployee />
-    </div>
+    <BrowserRouter>
+      <div className="bg-slate-500">
+        <Navbar />
+        <Routes>
+          <Route index element={<EmployeeList />} />
+          <Route path="/" element={<EmployeeList />}></Route>
+          <Route path="/employeeList" element={<EmployeeList />} />
+          <Route path="/addEmployee" element={<AddEmployee />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
